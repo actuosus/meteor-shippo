1d8c88027cc7c4e849bff0424cc42548d00141cb
Bozhao Yu <yubz86@gmail.com>
2014-07-14 13:19:40 -0700

    Use own functions instead relying on the nonexisting npm package.

--- package.js
+++ package.js
@@ -1,9 +1,11 @@
 Package.describe(" integration");
 
-Npm.depends({"shippo": "0.0.1"});
+Npm.depends({"when": "3.3.1"});
+//XXX Until they make it into npm we got go through this way :(
+//Npm.depends({"shippo": "0.0.1"});
 
 Package.on_use(function (api) {
-  api.add_files('shippo.js', 'client');
-
+  api.use('http', ['client', 'server']);
+  api.add_files('shippo.js', 'server');
   api.export('Shippo', 'server');
 });
--- shippo.js
+++ shippo.js
@@ -1,19 +1,127 @@
-var settings = Meteor.settings;
+/*
+ *  Shippo use POST and GET.  if you want to update data, just created another
+ *  one  (quote from their doc)
+ */
 
-var SHIPPO_USER = process.env.SHIPPO_USER || 
-  (settings && settings.shippo_user);
+var ShippoAPI = function (options) {
+  this.options = options || {};
+};
 
-var SHIPPO_PASSWORD = process.env.SHIPPO_PASSWORD || 
-  (settings && settings.shippo_password);
+ShippoAPI.prototype = {
+  configure: function (options) {
+    _.extend(this.options, options);
+    return this;
+  },
 
-if (!SHIPPO_USER)
-  throw new Error(
-    'SHIPPO_USER not set in Meteor.settings or an environment variable'
-  );
+  url: 'https://api.goshippo.com/v1/resource',
 
-if (!SHIPPO_PASSWORD)
-  throw new Error(
-    'SHIPPO_PASSWORD not set in Meteor.settings or an environment variable'
-  );
+  headers: function (options) {
+    options = options || {};
+    var auth = 'Basic ' +
+      new Buffer(this.options.userName + ':' + this.options.password).toString('base64');
+    return _.extend({
+      "Authorization": auth
+    }, options.headers || {});
+  },
 
-Shippo = Npm.require('shippo')(SHIPPO_USER, SHIPPO_PASSWORD);
+  request: function (method, resource, options) {
+    var url = this.url.replace('resource', resource);
+
+    var httpRequestOptions = {
+      params: options.params || {},
+      headers: this.headers(options)
+    };
+
+    if (options.data)
+      httpRequestOptions = _.extend(httpRequestOptions, {data: options.data});
+
+    var result = HTTP[method.toLowerCase()](url, httpRequestOptions);
+
+    if (result) {
+      var statusCode = result.statusCode;
+      if (statusCode.toString()[0] === '2') {
+        return result;
+      } else {
+        throw new Error('Shippo Request Error: ', result.statusCode, result);
+      }
+    }
+    if (result && result.statusCode === 201) {
+      return result;
+    }
+    else
+      throw new Error('Shippo Request Error: ', result.statusCode, result);
+  },
+
+  get: function (resource, options) {
+    return this.request('get', resource, options);
+  },
+
+  post: function (resource, options) {
+    return this.request('post', resource, options);
+  },
+
+  //business api
+  createAddress: function (data, options) {
+    var result = this.post('addresses/', _.extend({data: data}, options));
+    return result.data;
+  },
+
+  validateAddress: function (addressId, options) {
+    var url = 'addresses/' + addressId + '/validate';
+    var result = this.get(url, options);
+
+    return result.data;
+  },
+
+  validateNewAddress: function (data, options) {
+    var result = this.createAddress(data, options);
+
+    return this.validateAddress(result.object_id, options);
+  },
+
+  listAddresses: function (options) {
+    var result = this.get('addresses/', options);
+
+    return result.data;
+  },
+
+  createParcel: function (data, options) {
+    var result = this.post('parcels/', _.extend({data: data}, options));
+    return result.data;
+  },
+
+  createShipment: function (data, options) {
+    var result = this.post('shipments/', _.extend({data: data}, options));
+    return result.data;
+  },
+
+  //this is goiong to be used to create transaction
+  checkRate: function (data, options) {
+    var shipment = this.createShipment(data, options);
+
+    var url = 'shipments/' + result.object_id + '/rates/USD';
+    var result = this.get(url, options);
+
+    return result.data;
+  },
+
+  createTransaction: function (data, options) {
+    var addressFrom = this.createAddress(addressFrom, options);
+    var addressTo = this.createAddress(addressTo, options);
+    var shipment = _.extend(data.shipment, {
+      address_from: addressFrom.object_id,
+      address_to: addressTo.object_id
+    });
+    var rateData = this.checkRate(shipment, options);
+
+    var transactionData = _.extend(data.transaction, {rate: rateData.object_id});
+    var result = this.post('transactions/', transactionData, options);
+
+    return result.data;
+  }
+};
+
+Shippo = new ShippoAPI({
+  userName: Meteor.settings.shippo_user,
+  password: Meteor.settings.shippo_password,
+});
