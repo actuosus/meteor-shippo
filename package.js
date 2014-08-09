0a175509e6d78945bbf55a21d51faed870a575f5
Bozhao Yu <yubz86@gmail.com>
2014-08-08 23:12:08 -0700

    testing and preparing for 0.9

--- package.js
+++ package.js
@@ -1,10 +1,12 @@
-Package.describe(" integration");
-
-Npm.depends({"when": "3.3.1"});
-//XXX Until they make it into npm we got go through this way :(
-//Npm.depends({"shippo": "0.0.1"});
+Package.describe({
+  summary: "Shippo integration",
+  version: "0.0.3",
+  git: "https://github.com/yubozhao/meteor-shippo.git"
+});
 
 Package.on_use(function (api) {
+  api.versionsFrom("METEOR-CORE@0.9.0-atm");
+
   api.use('http', ['client', 'server']);
   api.add_files('shippo.js', 'server');
   api.export('Shippo', 'server');
